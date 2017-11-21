ce0e31293c314af647f45126c4d7063233988f55
Heena Patel <P14159492@LEC-ADMIN.dmu.ac.uk>
2017-11-21 13:33:55 +0000

    seperate pages; need to add content via javascript

---
+++ javascript/parse-data.js
@@ -0,0 +1,21 @@
+(function(){
+  let demoData = document.getElementById("demo");
+
+  // creates a JavaScript object by parsing the JSON-formatted string from the file
+  let obj = JSON.parse(jsonString);
+  console.log(obj);
+  
+  let theData = "";
+
+  
+  // populates the chosen element's content with the data:
+  
+  let i;
+  
+  for (i = 0; i < obj.content.length; i++) {
+	theData += `<li>${obj.content[i].points}</li>`;
+  }
+  
+  demoData.innerHTML = theData; 
+
+}());
