228c2117b910b57d5de7936d095b2655bb453850
Heena Patel <P14159492@LEC-ADMIN.dmu.ac.uk>
2017-12-02 12:22:46 +0000

    google map added in javascript

--- javascript/map.js
+++ javascript/map.js
@@ -1,8 +1,13 @@
 function myMap() {
-var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.12),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-}
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+    var grandyst = {lat: 52.63, lng: -1.13};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 5,
+     center: grandyst
+    });
+    var marker = new google.maps.Marker({
+        position: grandyst,
+        map: map
+      });    
+    }
+
+
