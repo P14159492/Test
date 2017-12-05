16952433f86452d8c160c1979149ff45dcfa647c
Heena Patel <P14159492@LEC-ADMIN.dmu.ac.uk>
2017-12-05 12:37:47 +0000

    pages formatted and text is showing unedr the images using javascript

--- javascript/data1.js
+++ javascript/data1.js
@@ -20,34 +20,4 @@ let jsonString = `
 	}
 ]}
 
-var richardthethird = `
-{"richardthird": [
-	{
-		"richardthirdpoints":"Richard was born on 2 October 1452 at Fotheringhay Castle in Northamptonshire. His father was Richard Plantagenet, Duke of York and his mother Cecily Neville."
-	},
-	{
-		"richardthirdpoints":"In August 1485, Henry Tudor, Earl of Richmond, who was a Lancastrian claimant to the throne living in France, landed in South Wales. He marched east and engaged Richard in battle on Bosworth Field in Leicestershire on 22 August."
-	},
-	{
-		"richardthirdpoints":"Although Richard possessed superior numbers, several of his key lieutenants defected. Refusing to flee, Richard was killed in battle and Henry Tudor took the throne as Henry VII."
-	},
-	{
-		"richardthirdpoints":"Richard’s body was brought back to Leicester, publicly displayed and then given for burial to a group of Franciscan friars."
-	},
-	{
-		"richardthirdpoints":"In September 2012, a team of archaeologists from Leicester University uncovered a body believed by some to be the remains of Richard III."
-	},
-	{
-		"richardthirdpoints":"The skeleton was uncovered at the former site of the Grey Friars church in Leicester, where Richard III was reportedly buried."
-	},
-	{
-		"richardthirdpoints":"The church had been destroyed in the 1530s, and the site had been a car parking lot in recent times."
-	},
-	{
-		"richardthirdpoints":"DNA samples were taken from the remains to be compared against a known descendant of Richard III's sister. In February 2013, the DNA results were revealed and verified the archaeological team's suspicions: The remains did, in fact, belong to Richard III."
-	},
-	{
-		"richardthirdpoints":"Shortly after the news broke, plans began for the body of the last Plantagent king to be re-interred. In March 2015, after a funeral that included a solemn ceremony filled with royal pageantry, Richard III was ultimately laid to rest at Leicester Cathedral, not far from the archaeologist's site and where he met his grisly end."
-	}
-]}
 `;
