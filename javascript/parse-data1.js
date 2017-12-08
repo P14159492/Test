81bac723cb44148a98a3d9aaddae4f0a6e39842e
David Everitt <DaveEveritt@users.noreply.github.com>
2017-12-08 17:28:58 +0000

    parse all the data for keylandmarks
    you can see how this works from the console log readout

--- javascript/parse-data1.js
+++ javascript/parse-data1.js
@@ -1,38 +1,31 @@
 (function(){
    
-  let demoData1 = document.getElementById("demo1");
-  let demoData2 = document.getElementById("demo2");
-  let demoData3 = document.getElementById("demo3");
-  let demoData4 = document.getElementById("demo4");
-  let demoData5 = document.getElementById("demo5");
-  let demoData6 = document.getElementById("demo6");
+  let demoData = [];
+  demoData[5] = document.getElementById("demo1");
+  demoData[4] = document.getElementById("demo2");
+  demoData[3] = document.getElementById("demo3");
+  demoData[2] = document.getElementById("demo4");
+  demoData[1] = document.getElementById("demo5");
+  demoData[0] = document.getElementById("demo6");
+
   // creates a JavaScript object by parsing the JSON-formatted string from the file
   let obj = JSON.parse(jsonString);
 
-  let theData1 =  "";
-  let theData2 =  "";
-  let theData3 =  "";
-  let theData4 =  "";
-  let theData5 =  "";
-  let theData6 =  "";
-  // populates the chosen element's content with the data:
-  
-  let i;
-  for (i = 0; i < obj.keylandmarks.length; i++){
-	theData1 += `<li>${obj.keylandmarks[i].leicestercatherdral}</li>`;
-	theData2 += `<li>${obj.keylandmarks[i].richardthirdpoints}</li>`;
-	theData3 += `<li>${obj.keylandmarks[i].clocktowerpoints}</li>`;
-	theData4 += `<li>${obj.keylandmarks[i].museumpoints}</li>`;
-	theData5 += `<li>${obj.keylandmarks[i].powerpoints}</li>`;
-	theData6 += `<li>${obj.keylandmarks[i].remembrancepoints}</li>`;
+  let i, j;
+  let theDataArray = [];
+
+  let len = Object.keys(obj.keylandmarks).length;
+  let n = len-1; // for counting down the demoData array
+
+  for (i in obj.keylandmarks){
+    let landmarks = obj.keylandmarks[i];
+    console.log(`\nOUTER LOOP: ${i}`);
+    for (key in landmarks) {
+      theDataArray[i] = `<li>${landmarks[key]}</li>`;
+      demoData[n].innerHTML += theDataArray[i];
+      console.log(`INNER LOOP:\n ${theDataArray[i]}`);
+      console.log(`counter(n) = ${n}, dataPoint(i) = ${i},\ndataPoint text(key) = ${key}`);
+    }
+    n -= 1;
   }
-  
-  demoData1.innerHTML = theData1; 
-  demoData2.innerHTML = theData2; 
-  demoData3.innerHTML = theData3;
-  demoData4.innerHTML = theData4;
-  demoData5.innerHTML = theData5;
-  demoData6.innerHTML = theData6;
 }());
-
-
